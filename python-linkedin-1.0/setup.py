f4448ea8315761cd0599687fbbe55656d346843c
ozgurvt <ozgurvt@46fc6e28-f406-11de-944c-65dca90e649d>
2009-12-30 22:41:06 +0000

    inital checkin
    git-svn-id: http://python-linkedin.googlecode.com/svn/trunk@2 46fc6e28-f406-11de-944c-65dca90e649d

---
+++ python-linkedin-1.0/setup.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name = "python-linkedin",
+      version = "1.0",
+      description = "Python LinkedIn API",
+      author = "Özgür Vatansever",
+      author_email = "ozgurvt@gmail.com",
+      url = "http://test.localhost",
+      packages = ["linkedin"])
