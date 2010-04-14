ff777dbf839a43b28f60b9d70fb0a7bcf2db9120
ozgurvt <ozgurvt@46fc6e28-f406-11de-944c-65dca90e649d>
2010-04-14 05:27:44 +0000

    setup script changed
    git-svn-id: http://python-linkedin.googlecode.com/svn/trunk@14 46fc6e28-f406-11de-944c-65dca90e649d

--- python-linkedin-1.5/setup.py
+++ python-linkedin-1.5/setup.py
@@ -3,8 +3,11 @@ from distutils.core import setup
 
 setup(name = "python-linkedin",
       version = "1.5",
-      description = "Python LinkedIn API",
+      description = "Python Interface for the LinkedIn API",
       author = "Özgür Vatansever",
+      licence = "LICENSE"
+      long_description = open("README").read(),
       author_email = "ozgurvt@gmail.com",
-      url = "http://localhost",
-      packages = ["linkedin"])
+      url = "http://code.google.com/p/python-linkedin/",
+      packages = ["linkedin"],
+      platforms = ["linux"])
