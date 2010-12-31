29ac21683637daad71a1f5a6e22719ee9f43b7c7
ptwobrussell <ptwobrussell@46fc6e28-f406-11de-944c-65dca90e649d>
2010-12-31 18:37:46 +0000

    Removing versioned directory information from trunk folder (would be best to create a tag for that) and added a __version__ global in linkedin.py that identifies trunk as a new version (1.6)
    git-svn-id: http://python-linkedin.googlecode.com/svn/trunk@22 46fc6e28-f406-11de-944c-65dca90e649d

--- linkedin/linkedin.py
+++ linkedin/linkedin.py
@@ -8,6 +8,8 @@
 # LinkedIn Account: http://www.linkedin.com/in/ozgurvt                                #
 #######################################################################################
 
+ __version__ = "1.6"
+
 """
 Provides a Pure Python LinkedIn API Interface.
 """
